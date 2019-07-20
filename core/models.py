2c4bc3eff5661b8f80c6652f6eb35d7be8a2edab
vitor-amartins <vmam@cin.ufpe.br>
2019-07-19 23:22:35 -0300

    Added choices on Link

--- core/models.py
+++ core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from core.utils import COLOR_CHOICES
 
 class Link(models.Model):
     link = models.CharField("Link", max_length=200)
     descricao = models.CharField("Descrição", max_length=100)
     ordem = models.PositiveSmallIntegerField("Ordem", null=True, blank=True)
-    cor = models.PositiveSmallIntegerField("Cor")
+    cor = models.PositiveSmallIntegerField("Cor", choices=COLOR_CHOICES)
 
     class Meta:
         verbose_name = "Link"
--- core/utils.py
+++ core/utils.py
@@ -3,3 +3,10 @@ def get_color(value):
     if value >= 0 and value < len(COLORS):
         return COLORS[value]
     return None
+
+COLOR_CHOICES = (
+    (0, get_color(0)),
+    (1, get_color(1)),
+    (2, get_color(2)),
+    (3, get_color(3))
+)
