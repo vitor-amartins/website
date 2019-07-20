6440898b58e0ab161ed387982dd20bfd358b3731
vitor-amartins <vmam@cin.ufpe.br>
2019-07-19 23:30:08 -0300

    Testing whitenoise

--- website/settings/base.py
+++ website/settings/base.py
@@ -99,4 +99,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
